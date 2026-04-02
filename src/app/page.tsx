d0ac8f7661921676c4299e586a39190a378013a8
Basharat Salam` <68460973+shahbasharat@users.noreply.github.com>
2026-04-02 15:08:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-
-import ScrollyCanvas from "../components/ScrollyCanvas";
-import Overlay from "../components/Overlay";
-import Projects from "../components/Projects";
-import About from "../components/About";
-import Experience from "../components/Experience";
-import Skills from "../components/Skills";
-import Education from "../components/Education";
-import Contact from "../components/Contact";
+import ScrollyCanvas from "@/components/ScrollyCanvas";
+import Overlay from "@/components/Overlay";
+import Projects from "@/components/Projects";
+import About from "@/components/About";
+import Experience from "@/components/Experience";
+import Skills from "@/components/Skills";
+import Education from "@/components/Education";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -16,7 +15,6 @@ export default function Home() {
         <ScrollyCanvas />
         <Overlay />
       </div>
-
       <div className="relative z-20 bg-black">
         <About />
         <Experience />
